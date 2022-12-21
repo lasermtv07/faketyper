5e9f9ba82d970758e7dc77b53dab6a90a2b6c443
lasermtv07 <118477750+lasermtv07@users.noreply.github.com>
2022-12-21 22:02:18 +0100

    Update control.py
    21_12_22 - commit

--- control.py
+++ control.py
@@ -2,8 +2,9 @@ import sys
 import os.path
 import typer
 import time
+history=[]
 def help(sw,args):
-    if "/s" in sw:
+    if "s" in sw:
         print('''
         HELP - SHORT
         ============
@@ -41,7 +42,7 @@ def help(sw,args):
             -/c prints more detailed version of version changelog
         ''')
 def version(sw,args):
-    if "/c" in sw:
+    if "c" in sw:
         print(''' 
        =========
        =VERSION=
@@ -69,60 +70,64 @@ def version(sw,args):
 Still under development. Use version /c for more info
 or help for help''')
 def run(sw,args):
+    vol=' '
     onscreen_kb=1
     use_cap=1
     write_out=0
-    if "/k" in sw:
+    if "k" in sw:
         onscreen_kb=0
-    if "/d" in sw:
+    if "d" in sw:
         use_cap=0
-    if "/b" in sw and "/k" in sw:
+    if "b" in sw and "k" in sw:
         write_out=1
     if args[0].isnumeric()!=True:
         print("Error:run command:wrong input type (speed)")
     if args[1].isnumeric()!=True and int(args[1])<101:
         print("Error:run command:wrong input type (errors)")
-    if "/w" not in sw:
+    if "w" not in sw:
         print("wait 3 seconds")
         time.sleep(3)
-    try: typer.typer(args[2],int(args[0]),int(args[1]),args[3],onscreen_kb,use_cap,write_out)
+    try: vol=args[3]
+    except: vol=' '
+    try: typer.typer(args[2],int(args[0]),int(args[1]),vol,onscreen_kb,use_cap,write_out)
     except: print("Error:run command:unknown error")
 
-
 class Command:
-    def __init__(self, cmd, switch, args, execute):
+    def __init__(self, cmd, alias, args,vol, execute):
         self.cmd=cmd
-        self.switch=switch
+        self.alias=alias
         self.args=args
         self.execute=execute
-
+        self.vol=vol
     def update(self):
-        if len(sys.argv)>1 and self.cmd==sys.argv[1]:
+        if len(sys.argv)>1 and (self.cmd==sys.argv[1] or self.alias==sys.argv[1]):
             receiveargs=[]
             out_sw=[]
             for x in range(2,len(sys.argv)):
                 receiveargs.append(sys.argv[x])
             for y in receiveargs:
-                if y in self.switch and y[0]=="/" and y not in out_sw:
+                if y[0]=="/" or y[0]=="-":
                     out_sw.append(y)
-                elif y[0]=="/" and y not in out_sw and y[1] in self.switch:
-                    print("Error:Wrong switches useds")
-                    exit()
             for r in out_sw:
                 while r in receiveargs:
                     receiveargs.remove(r)
+            for u in range(len(out_sw)):
+                if "/" in out_sw[u] or "-" in out_sw[u]:
+                    out_sw[u]=out_sw[u][1]
             #print(str(len(receiveargs)) + str(receiveargs))
-            if len(receiveargs)==self.args:
+            if len(receiveargs)==self.args or len(receiveargs)>self.args:
                 exec(self.execute)
-            else: print("Error:Incorrect number of argument")
+            else: print("Error:Incorrect number of arguments")
 
+if len(sys.argv)==1:
+    version([],0)
 
-hi=Command("hi", ["/a"], 3,'print("hi" + str(out_sw) + str(receiveargs))')
-hi.update()
-r=Command("run", ["/k","/d","/w","/b"], 4,'run(out_sw,receiveargs)')
+#hi=Command("hi", ["/a"], 3,1,'print("hi" + str(out_sw) + str(receiveargs))')
+#hi.update()
+r=Command("run", "r", 3,1,'run(out_sw,receiveargs)')
 r.update()
-v=Command("help", ["/s"], 0,'help(out_sw,0)')
+v=Command("help", "h", 0,0,'help(out_sw,0)')
 v.update()
-x=Command("version", ["/c"], 0,'version(out_sw,0)')
+x=Command("version", "v", 0,0,'version(out_sw,0)')
 x.update()
 
